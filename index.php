4930382d8ed7895339516662b4e8e8dc7525ee4b
SIDDIK <mds569092@gmail.com>
2021-10-29 00:10:07 +0600

    contact form create

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact form | php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-5 col-xxl-5 col-10 mx-auto mt-5">
+                <form action="/contact_form/" method="POST">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Enter Name</label>
+                        <input type="text" class="form-control" id="name" name="name" aria-describedby="emailHelp" autocomplete="off">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" autocomplete="off">
+                    </div>
+                    <div class="mb-3">
+                        <label for="subject" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="subject" name="subject" aria-describedby="emailHelp" autocomplete="off">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message" class="form-label">Description</label>
+                        <textarea class="form-control" name="message" id="message" rows="4" autocomplete="off"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
